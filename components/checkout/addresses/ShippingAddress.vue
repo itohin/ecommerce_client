3ee525ff816fe5e8fae46e5877b621397e2f43a5
itohin <buhtig5102$>
2020-03-28 14:48:47 +0300

    Change shipping address

--- components/checkout/addresses/ShippingAddress.vue
+++ components/checkout/addresses/ShippingAddress.vue
@@ -3,12 +3,32 @@
         <div class="message-body">
             <h1 class="title is-5">Ship to</h1>
 
-            <template v-if="selectedAddress">
-                {{ selectedAddress.name }} <br>
-                {{ selectedAddress.address_1 }} <br>
-                {{ selectedAddress.city }} <br>
-                {{ selectedAddress.postal_code }} <br>
-                {{ selectedAddress.country.name }} <br>
+            <template v-if="selecting">
+                <ShippingAddressSelector
+                        :addresses="addresses"
+                        :selectedAddress="selectedAddress"
+                        @click="addressSelected"
+                />
+            </template>
+
+            <template v-else>
+                <template v-if="selectedAddress">
+                    <p>
+                        {{ selectedAddress.name }} <br>
+                        {{ selectedAddress.address_1 }} <br>
+                        {{ selectedAddress.city }} <br>
+                        {{ selectedAddress.postal_code }} <br>
+                        {{ selectedAddress.country.name }} <br>
+                    </p>
+                    <br>
+                </template>
+
+                <div class="field is-grouped">
+                    <p class="control">
+                        <a href="" class="button is-info" @click.prevent="selecting = true">Change shipping address</a>
+                    </p>
+                </div>
+
             </template>
 
         </div>
@@ -17,13 +37,20 @@
 
 <script>
 
+    import ShippingAddressSelector from './ShippingAddressSelector';
+
     export default {
 
         data () {
             return {
+                selecting: false,
                 localAddress: this.addresses,
                 selectedAddress: null
             }
+        },
+
+        components: {
+            ShippingAddressSelector
         },
 
         props: {
@@ -40,6 +67,10 @@
         },
 
         methods: {
+            addressSelected (address) {
+                this.switchAddress(address);
+                this.selecting = false;
+            },
             switchAddress (address) {
                 this.selectedAddress = address;
             }
